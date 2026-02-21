e1f3830e92d524900da20d720ffa5f257daa7d5e
Gilang Rizki Ramadhan <gilangrizkiramadhan1906@gmail.com>
2026-02-22 01:03:07 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
+const repoName = "portfolio"; // <-- ganti sesuai nama repo kamu
+
 const nextConfig = {
-  output: "export", // <--- TAMBAHIN INI
+  output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? `/${repoName}` : "", // penting!
+  assetPrefix: isProd ? `/${repoName}/` : "", // penting banget buat _next/static dll
 };
 
 export default nextConfig;
